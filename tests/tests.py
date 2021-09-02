4c4b5f853706414fb4d698bda770d0191d32c83d
Thomas Trividic <thomas.trividic@prestashop.com>
2021-09-02 10:34:15 +0200

    ok

--- tests/tests.py
+++
@@ -1,9 +0,0 @@
-from ascend_great_expectations_gcs_test.validator import TestValidator
-# from ascend_great_expectations_gcs.validator import GEValidator
-from ascend_great_expectations_gcs.validator import GEValidator
-
-with open("../.secrets/credentials.json", "r") as file:
-    credentials = file.read()
-    # validator = TestValidator(credentials=credentials)
-    # # validator.test_run()
-    # validator.test_run_checkpoint()
